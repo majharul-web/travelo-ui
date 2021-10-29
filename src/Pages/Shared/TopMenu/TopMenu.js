3235d88250522b7cd0eb283983f44b5f108949e0
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-29 15:35:50 +0600

    add header

--- src/Pages/Shared/TopMenu/TopMenu.js
+++ src/Pages/Shared/TopMenu/TopMenu.js
@@ -2,36 +2,36 @@ import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import logo from '../../../images/logos/logo.png'
 
 const TopMenu = () => {
     const { user, logOut } = useAuth();
     return (
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-            <Container>
+        <Navbar collapseOnSelect expand="lg" variant="light">
+            <Container fluid className='mx-5'>
 
                 <Navbar.Brand >
                     <Link to='home'>
-                        {/* <img src={logo} className='img-fluid w-100' alt="" /> */}
-                        React
+                        <img src={logo} className='img-fluid w-100' alt="" />
                     </Link>
                 </Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto ">
-                        <Nav.Link as={Link} to="/home" active className='fw-bold  text-primary mx-2'>Home</Nav.Link>
+                    <Nav className="me-auto ">
+                        <Nav.Link as={Link} to="/home" active className=' mx-1'>Home</Nav.Link>
 
-                        <Nav.Link as={Link} to="/services" active className='fw-bold  text-primary mx-2'>Services</Nav.Link>
+                        <Nav.Link as={Link} to="/packages" active className=' mx-1'>Packages</Nav.Link>
 
-                        <Nav.Link as={Link} to="/about" active className='fw-bold  text-primary mx-2'>About Us</Nav.Link>
+                        <Nav.Link as={Link} to="/about" active className=' mx-1'>About</Nav.Link>
 
-                        <Nav.Link as={Link} to="/career" active className='fw-bold  text-primary mx-2'>Career</Nav.Link>
+                    </Nav>
+
+                    <Nav>
                         {
-                            user?.email && <Nav.Link as={Link} to="/orders" active className='fw-bold  text-primary mx-2'>My Orders</Nav.Link>
+                            user?.email && <Nav.Link as={Link} to="/orders" active className=' mx-1'>My Orders</Nav.Link>
                         }
-
-
 
                         {
                             user?.displayName ?
@@ -45,15 +45,11 @@ const TopMenu = () => {
                         }
                         {
                             user?.displayName &&
-                            <Nav.Link className='fw-bold  text-primary mx-2'>
-                                <span className='text-danger mx-2'><i className="fas fa-user-check"></i></span>
+                            <Nav.Link className=' mx-2'>
+                                <span className='text-danger mx-1'><i className="fas fa-user-check"></i></span>
                                 {user?.displayName}
                             </Nav.Link>
                         }
-
-
-
-
                     </Nav>
                 </Navbar.Collapse>
 
