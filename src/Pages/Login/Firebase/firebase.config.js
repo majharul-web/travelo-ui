92cfe23727ea9c71bc847233818927f349d41843
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-29 20:20:08 +0600

    fixed some issues

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,23 +1,12 @@
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDlJNyDuFFYZ54HMqfzrG2iWRQFMGP3PHE",
-    authDomain: "start-firebase-learn.firebaseapp.com",
-    projectId: "start-firebase-learn",
-    storageBucket: "start-firebase-learn.appspot.com",
-    messagingSenderId: "41963249055",
-    appId: "1:41963249055:web:a6213275762737775ff1aa"
+    apiKey: "AIzaSyA4Z1pq6YoYo9nni9Y-JWNZiyo11YfrBAQ",
+    authDomain: "travelo-2031c.firebaseapp.com",
+    projectId: "travelo-2031c",
+    storageBucket: "travelo-2031c.appspot.com",
+    messagingSenderId: "1046115829766",
+    appId: "1:1046115829766:web:930d60a4a96effb6105b87"
 };
+
 export default firebaseConfig;
-
-
-
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_API_ID,
-// };
-
-// export default firebaseConfig;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -31,28 +31,28 @@ const Footer = () => {
                                     <ul>
                                         <li>
                                             <a href="/">
-                                                <i class="fab fa-facebook-f"></i>
+                                                <i className="fab fa-facebook-f"></i>
                                             </a>
                                         </li>
                                         <li>
                                             <a href="/">
-                                                <i class="fab fa-twitter"></i>
+                                                <i className="fab fa-twitter"></i>
                                             </a>
                                         </li>
                                         <li>
                                             <a href="/">
-                                                <i class="fab fa-instagram"></i>
+                                                <i className="fab fa-instagram"></i>
                                             </a>
                                         </li>
 
                                         <li>
                                             <a href="/">
-                                                <i class="fab fa-youtube"></i>
+                                                <i className="fab fa-youtube"></i>
                                             </a>
                                         </li>
                                         <li>
                                             <a href="/">
-                                                <i class="fab fa-pinterest-p"></i>
+                                                <i className="fab fa-pinterest-p"></i>
                                             </a>
                                         </li>
 
