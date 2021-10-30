66cf60dc169d3abcdb9173ae0ffbd0a05a22463e
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-30 10:14:22 +0600

    add destination form

--- src/Pages/AddDestination/AddDestination.js
+++ src/Pages/AddDestination/AddDestination.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
 
 const AddDestination = () => {
+    const { user } = useAuth();
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
-        <div>
-            <h3>add new </h3>
+        <div className='container py-4'>
+            <div className='addUser'>
+                <h3 className='text-center text-danger py-3'>Add Travel Destination</h3>
+                <form className='d-flex flex-column justify-content-center align-items-center ' onSubmit={handleSubmit(onSubmit)}>
+
+                    <input className='w-50 form-control p-2 mb-3' type='text' defaultValue={user?.displayName} {...register("name")} />
+                    <input className='w-50 form-control p-2 mb-3' type='email' defaultValue={user?.email} {...register("email")} />
+
+                    <input className='w-50 form-control p-2 mb-3' type="text" {...register("destination")} placeholder='Destination' />
+                    <input className='w-50 form-control p-2 mb-3' type="number" {...register("cost")} placeholder='Travel Cost' />
+
+                    <textarea className="w-50 form-control p-2 mb-3" cols="10" rows="5" placeholder='Details...' {...register("details")}></textarea>
+                    <input className='w-50 form-control p-2 mb-3' type='text'  {...register("imgURL")} placeholder='image link' />
+                    <input className='w-50 form-control p-2 mb-3 btn btn-danger' type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
